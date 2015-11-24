0808311c84326dac0acd46255c2efe43413ba516
Renaud <rgiraud-sauveur@actemium.com>
2015-11-24 13:58:28 +0100

    fakeId problem solved

--- src/main/java/utils/champ/ChampUid.java
+++ src/main/java/utils/champ/ChampUid.java
@@ -1,27 +1,31 @@
 package utils.champ;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class ChampUid extends Champ {
 
 	public static String uidFieldName = "id";
-	public UUID uuid;
+	
+	private static Map<Object, UUID> dicoObjToFakeId = new HashMap<>();
 	
 	ChampUid() {
 		super(null, true);
 		name = uidFieldName;
-		uuid = UUID.randomUUID();
 		valueType = UUID.class;
 	}
 	
 	@Override
 	public  void set(Object obj, Object value) {
-		uuid = (UUID) value;
+		dicoObjToFakeId.put(obj, (UUID)value);
 	}
 
 	@Override
 	public UUID get(Object obj) {
-		return uuid;
+		if(!dicoObjToFakeId.containsKey(obj))
+			dicoObjToFakeId.put(obj, UUID.randomUUID());
+		return dicoObjToFakeId.get(obj);
 	}
 
 	@Override
